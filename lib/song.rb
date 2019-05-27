8ef44b09c6f0bf757faa695a7d341d12bc66179c
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-27 00:09:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
@@ -33,7 +34,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+    #name.downcase.gsub(' ', '-')
+  #end
 end
