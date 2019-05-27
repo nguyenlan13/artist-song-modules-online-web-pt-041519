d6648218599d199fb45eaa002155affdee40b85c
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-27 00:19:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,15 +5,16 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
+  #def initialize
     
     #@@songs << self
-  end
+  #end
 
   #def self.find_by_name(name)
     #@@songs.detect{|a| a.name == name}
